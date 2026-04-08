a93fad5da3577b657aca2c87a1b4beb72b25fdf0
tejas <tejas140905@gmail.com>
2026-04-08 12:30:02 +0530

    Update hero heading and typing effect with new text

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ const newsletterHandler = () => {
 
 // Typing Effect
 const typeEffect = () => {
-    const text = "Sarvatah Foundation Development for Everyone. Everyone Deserves to Grow";
+    const text = "Sarvatah Foundation, Education for Everyone. Everyone Deserves to Growth";
     const h1 = document.getElementById('typing-h1');
     let i = 0;
     let isDeleting = false;
